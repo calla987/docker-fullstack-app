fd21956b1eda4b6d9302baa54cfe8ade7b25495d
VincentCode <calla987@gmail.com>
2020-11-11 22:41:46 +0900

    fullstack 파일 업로드

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+// import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const db= require('./db');
+
+// Express create server
+const app =  express();
+
+// json 형태 parser 자동 해석
+app.use(bodyParser.json);
+
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fileds) => {
+    console.log('results', results)
+})
+
+
+//DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기
+app.get('/api/hi', function (req, res) {
+    //데이테베이스에서 모든 정보 가져오기
+    res.status(200).send('good')
+})
+
+
+app.get('/api/values',function(req,res){
+   db.pool.query('SELECT * FROM lists;',
+       (err,results,fields) => {
+       if(err)
+           return res.status(500).send(err)
+       else
+           return res.json(results)
+   })
+});
+
+app.post('/api/value',function(req,res,next){
+    db.pool.query('INSERT INTO  lists (value) VALUES("${req.body.value}") ;',
+        (err,results,fields) => {
+            if(err)
+                return res.status(500).send(err)
+            else
+                return res.json({ success: true , value: req.body.value})
+        })
+});
+
+
+app.listen(5000,() =>{
+   console.log('Backend App running 5000 port !!!')
+});
